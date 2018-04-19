f06cf88c6128f82f26e8f54d389b2dc4dd3d5ce2
Shae <smuller@udel.edu>
2018-04-19 10:23:42 -0400

    4-19-18 commit

---
+++ 4-19-18/python-basics.py
@@ -0,0 +1,8 @@
+greeting = "Hello"
+name = "Shae"
+print greeting ## this prints out a string
+print len(greeting)
+print greeting.upper()
+print greeting[3]
+print greeting + " " + name + "!"
+print "%s %s!" % (greeting, name) ## string formating
