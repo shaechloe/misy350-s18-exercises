59cf97cdb83ba6f81882a4a31082ccab2597fdeb
Shae <smuller@udel.edu>
2018-05-01 10:39:00 -0400

    this is my nav bar and stuff

--- songbase/songbase.py
+++ songbase/songbase.py
@@ -1,15 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "<h1>hello shae</h1>" ##this returns html
+    ## return "<h1>hello shae</h1>" ##this returns html
+    return render_template('index.html')
+
+
+@app.route('/users/<string:username>/')
+def users(username):
+    #return "hello" + username
+    ##return "<h1>hello %s</h1>" % username
+    return render_template('user.html', uname=username)
+
+
+@app.route('/songs')
+def shaw_all_songs():
+        songs = [
+        'Paradise',
+        'Yellow',
+        'Viva La Vida'
+        ]
+        return render_template ('song-all.html', songs=songs)
 
 
 @app.route('/user')
 def user():
-    return "this is the page for users"
+        return "this is the page for users"
 
 
 if __name__ == '__main__':
